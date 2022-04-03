6bcb1598d4879f494c70048f3470079b9bc260f2
游竣量 <jim@youjunliangdeMacBook-Pro.local>
2022-04-03 18:40:02 +0800

    1.0

--- HW1/HW1.ino
+++ HW1/HW1.ino
@@ -1,17 +1,15 @@
-void setup()
-{
+void setup() {
   Serial.begin(9600);
   pinMode(A0, INPUT);
   pinMode(A5, INPUT);
 }
 
-void loop()
-{
+void loop() {
   float V = analogRead(A5);
   Serial.println(V);
   // 假設 V 大於 544 是白天
   // 啟動 TMP 偵測當前溫度
-  if(V > 544){
+  if(V > 544) {
   	float TMP = analogRead(A0);
   	// 官方公式換算
   	int T = int(((TMP * 5000 / 1024) - 500) / 10); 
